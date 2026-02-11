e9592fb636b0d0285910fd644caafc29a6df8a08
tmdwo1713 <lsjlyl0511@gmail.com>
2026-02-11 15:08:16 +0900

    소설 에피소드,뷰어,작성 페이지 제작

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -11,14 +11,43 @@ public class MainController {
 		return "index";
 	}
 	
-<<<<<<< HEAD
-	@GetMapping("/seungjae")
-	public String seungjae() {
-		return "seungjae";
-=======
+	@GetMapping("/novel_list")
+	public String novel_list() {
+		return "novel_list";
+	}
+	
+	@GetMapping("/novel_detail")
+	public String novel_detail() {
+		return "novel_detail";
+	}
+	
+	@GetMapping("/novel_viewer")
+	public String novel_viewer() {
+		return "novel_viewer";
+	}
+	
+	@GetMapping("/novel_episode_write")
+	public String novel_episode_write() {
+		return "novel_episode_write";
+	}
+	
+	@GetMapping("/novel_write")
+	public String novel_write() {
+		return "novel_write";
+	}
+	
+	@GetMapping("/mypage_home")
+	public String mypage_home() {
+		return "mypage_home";
+	}
+	
+	@GetMapping("/sungmin") 
+	public String sungmin() {
+		return "index";
+	}
+
 	@GetMapping("/teamzang")
 	public String teamzang() {
 		return "teamzang";
->>>>>>> branch 'develop' of https://github.com/creatival/creatival.git
 	}
 }
